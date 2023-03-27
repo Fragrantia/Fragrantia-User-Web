d78969d397f5fd98fea3ab61c1987bdfe40fa86b
clldflwr <dlwjddlr97@gmail.com>
2023-03-27 16:02:40 +0900

    IK-1 헤더수정 및 로고 이미지 추가

--- js/HeaderToggle.js
+++ js/HeaderToggle.js
@@ -1,5 +1,13 @@
-jQuery(function($){
-  $(".menu-toggle-btn").click(function(){
-      $(".main_nav").stop().slideToggle("fast");
-  });
+// 수정 전 : 위아래 슬라이드
+// jQuery(function($){
+//   $(".menu-toggle-btn").click(function(){
+//       $(".main_nav").stop().slideToggle("fast");
+//   });
+// }); 
+
+// 수정 후 : 좌우 슬라이드
+$(document).ready(function(){
+	$('.menu-toggle-btn').click(function(){
+   		$('.main_nav').animate({width:"toggle"},200);
+	});
 });
