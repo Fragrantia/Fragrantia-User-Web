081c0278e506de0305c6ceb5bf61fddfd9b3959c
hj <gksgpwls0306@naver.com>
2023-03-28 13:09:17 +0900

    HJ-2 사이드 바 토글작업

--- js/fProductPageModuleSetting.js
+++ js/fProductPageModuleSetting.js
@@ -2,4 +2,4 @@ $(function(){
     $('#sidebarF').load('../module/sidebarF.html');
     $('#header').load('../module/header.html');
     $('#footer').load('../module/footer.html');
-})
+})//최종 사용하는 html기준으로 로그 경로 지정
