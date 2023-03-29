9cf68473496c2445d143cf3d8d4253ffff6bd7ba
Jun, Ahah <96584832+peubel@users.noreply.github.com>
2023-03-29 16:57:56 +0900

    JUN-17 sidebar fadeToggle 적용

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,6 @@
-// $(function(){
-//     $('#sidebarFlower').load('../module/sidebarF.html');
-// })//최종 사용하는 html기준으로 로그 경로 지정
-
 const toggleFilter = document.querySelector('.toggleFilter');
 const filter = document.querySelector('.filter');
 
 toggleFilter.addEventListener('click', () => {
-  filter.classList.toggle('show');
+  $(filter).fadeToggle();
 });
-
