58981e0b4180d1b36298be87e3da1bc6db7af39e
Jun, Ahah <96584832+peubel@users.noreply.github.com>
2023-03-30 12:35:11 +0900

    JUN-21 모듈 전체적인 수정 및 페이지 모듈 전용 js 분리

--- js/pageModuleSetting.js
+++ js/pageModuleSetting.js
@@ -1,4 +1,28 @@
-$(function() {
-    $('#header').load('../module/header.html');
-    $('#footer').load('../module/footer.html');
-});
+function loadModule(element) {
+    let targetElement = '#' + element;
+
+    let css = '../../css/module/' + element + '.css';
+    let html = '../../html/module/' + element + '.html';
+
+    if (element.includes("sidebar"))
+        css = '../../css/module/sidebar.css';
+
+    $.get(css, function (data) {
+        $('head').append('<style class="' + element + '" >' + data + '</style>');
+        $(targetElement).load(html);
+    });
+}
+
+$(function () {
+    loadModule('header')
+    loadModule('footer')
+
+    if (document.title.toLowerCase().includes("flower")) {
+        loadModule('sidebarFlower')
+        loadModule('topButton')
+    }
+    if (document.title.toLowerCase().includes("perfume")) {
+        loadModule('sidebarPerfume')
+        loadModule('topButton')
+    }
+})
