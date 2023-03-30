a44b137fad52899daee9e3b837af9f4d06d70eb4
Jun, Ahah <96584832+peubel@users.noreply.github.com>
2023-03-30 17:51:02 +0900

    JUN-24 헤더 토글 다른곳 누르면 닫히게 개발

--- js/headerToggle.js
+++ js/headerToggle.js
@@ -1,13 +1,14 @@
-// 수정 전 : 위아래 슬라이드
-// jQuery(function($){
-//   $(".menu-toggle-btn").click(function(){
-//       $(".main_nav").stop().slideToggle("fast");
-//   });
-// }); 
-
-// 수정 후 : 좌우 슬라이드
 $(document).ready(function(){
+	// 메뉴 토글 버튼 클릭 시 메뉴 토글
 	$('.menu-toggle-btn').click(function(){
-   		$('.main_nav').animate({width:"toggle"},200);
+		$('.main_nav').animate({width:"toggle"}, 200);
+	});
+
+	// 문서의 다른 영역 클릭 시 메뉴 닫기
+	$(document).click(function(event){
+		// 메뉴가 열린 상태에서만 메뉴 외부 요소를 클릭한 경우 메뉴 닫기
+		if($('.main_nav').is(':visible') && !$(event.target).closest('.main_nav, .menu-toggle-btn').length){
+			$('.main_nav').animate({width:"toggle"}, 50);
+		}
 	});
 });
